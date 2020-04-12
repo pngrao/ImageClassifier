140b05c9ced8c9ff4a7796ae8cb2ad00078029d8
Pannaga Rao <pannaga.murthy@gmail.com>
2020-04-12 00:33:19 -0400

    Fixed issues in running predict on Windows10

--- network_utils.py
+++ network_utils.py
@@ -1,12 +1,12 @@
 '''
-network_utils.py - Performs loading of datasets, 
+network_utils.py - Performs loading of datasets,
                    sets network parameters,
                    Trains the model and validates the training,
-                   tests the accuracy of trained model, 
+                   tests the accuracy of trained model,
                    saves checkpoint,
-                   loads checkpoint, 
+                   loads checkpoint,
                    processes an image,
-                   predicts most probable image categories for a given image           
+                   predicts most probable image categories for a given image
 '''
 #Import relevant packages
 import torch
@@ -18,12 +18,12 @@ from PIL import Image
 import numpy as np
 
 #Global settings
-    
-#Define a dictionary mapping of Pretrained Networks to a list of 
+
+#Define a dictionary mapping of Pretrained Networks to a list of
 #their input features and torchvision command to get the respective models.
 nn_list = {'vgg16' : [25088, "models.vgg16(pretrained=True)"],
            'densenet121' : [1024, "models.densenet121(pretrained=True)"],
-           'densenet201' : [1920, "models.densenet201(pretrained=True)"]} 
+           'densenet201' : [1920, "models.densenet201(pretrained=True)"]}
 
 #Number of flower classes form the output layer features
 output_count = 102
@@ -33,14 +33,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_dataloaders(data_dir):
     '''
-    Perform training data augmentation, data normalization, loading and batching 
+    Perform training data augmentation, data normalization, loading and batching
     for the training, validation, and testing sets
     '''
     #Define dataset directories
     train_dir = data_dir + '/train'
     valid_dir = data_dir + '/valid'
     test_dir = data_dir + '/test'
-    
+
     train_transform = transforms.Compose([transforms.RandomRotation(30),
                                           transforms.RandomResizedCrop(224),
                                           transforms.RandomHorizontalFlip(),
@@ -84,7 +84,7 @@ def set_network_params(pt_network, hidden_count, p, lr, is_gpu):
     '''
     input_count = nn_list[pt_network][0]
     model = eval(nn_list[pt_network][1])
-    
+
     #Turn of autograd
     for param in model.parameters():
         param.requires_grad=False
@@ -100,19 +100,19 @@ def set_network_params(pt_network, hidden_count, p, lr, is_gpu):
                                           ('hidden3', nn.Linear(hidden_count[2], output_count)),
                                           ('output', nn.LogSoftmax(dim=1))]))
     model.classifier = Netifier
-    
+
     #Move the newely defined model to available device
     if str(device)=='cuda' and is_gpu:
         model.to(device);
     #else:#cpu
         #model.to(device);
-        
-    #Define criterion 
+
+    #Define criterion
     criterion = nn.NLLLoss()
 
-    #Define optimizer 
+    #Define optimizer
     optimizer = optim.Adam(model.classifier.parameters(), lr)
-    
+
     return model, criterion, optimizer
 
 def validate_network(model, criterion, optimizer, dataloader, is_gpu):
@@ -121,7 +121,7 @@ def validate_network(model, criterion, optimizer, dataloader, is_gpu):
                        Indicates how well our model is training.
                        Inputs:
                        model, criterion, optimizer, dataloader, is_gpu
-                       Returns Loss and Accuracy 
+                       Returns Loss and Accuracy
     '''
     loss = 0
     accuracy = 0
@@ -137,7 +137,7 @@ def validate_network(model, criterion, optimizer, dataloader, is_gpu):
                 images, labels = images.to(device), labels.to(device)
             #Do a forward pass and get log probability
             output = model.forward(images)
-            #Compute running loss 
+            #Compute running loss
             loss += criterion(output, labels).item()
             #Get linear probability
             prob_output = torch.exp(output)
@@ -153,9 +153,9 @@ def validate_network(model, criterion, optimizer, dataloader, is_gpu):
 
 def train_and_validate_network(model, criterion, optimizer, trainloader, validateloader, epochs, is_gpu):
     '''
-    train_and_validate_network - Runs Training loop with training dataset and computes training loss. 
+    train_and_validate_network - Runs Training loop with training dataset and computes training loss.
                                  Every fixed interval performs validation to compute testing loss and accuracy
-                                 Inputs: 
+                                 Inputs:
                                  model, criterion, optimizer, trainloader, validateloader, epochs, is_gpu
                                  Output:
                                  Prints Training loss, validation loss and validation accuracy
@@ -186,11 +186,11 @@ def train_and_validate_network(model, criterion, optimizer, trainloader, validat
                 if step%print_interval==0:
                     #Validate network performance and get loss and accuracy
                     vloss, vaccuracy = validate_network(model, criterion, optimizer, validateloader, is_gpu)
-                    print(f"Epoch {e+1}/{epochs} : " 
+                    print(f"Epoch {e+1}/{epochs} : "
                           f"Training Loss = {running_loss/len(trainloader):.3f}, "
                           f"Validation Loss = {vloss/len(validateloader):.3f}, "
                           f"Validation Accuracy = {vaccuracy/len(validateloader)*100:.3f}%")
-                
+
         print("\n***Hurray! Model Training complete***\n")
 
 
@@ -209,7 +209,7 @@ def network_test_accuracy_checker(model, criterion, optimizer, testloader, is_gp
 
 def save_checkpoint(model, train_dataset, pt_network, hidden_count, cp_dir):
     '''
-    Save the checkpoint 
+    Save the checkpoint
     '''
     model.class_to_idx = train_dataset.class_to_idx
     checkpoint = {'model_name' : pt_network,
@@ -222,7 +222,7 @@ def load_checkpoint(cp_file, is_gpu):
     '''
     Load checkpoint and rebuild the model
     '''
-    checkpoint = torch.load(cp_file)
+    checkpoint = torch.load(cp_file, map_location=device)
     model_name = checkpoint['model_name']
     model,_,_ = set_network_params(model_name, checkpoint['hidden_layers_size'], 0.2, 0.03, is_gpu)
     model.class_to_idx = checkpoint['class_to_idx']
@@ -230,23 +230,25 @@ def load_checkpoint(cp_file, is_gpu):
     return model
 
 def process_image(image_path):
-    ''' 
+    '''
     Scales, crops, and normalizes a PIL image for a PyTorch model,
     Input - Full image path
     Returns a torch tensor
     '''
-    pil_image = Image.open(image_path)   
+    pil_image = Image.open(image_path)
     transform = transforms.Compose([transforms.Resize(256),
                                       transforms.CenterCrop(224),
                                       transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                      transforms.Normalize([0.485, 0.456, 0.406],
                                                            [0.229, 0.224, 0.225])])
     final_image = transform(pil_image)
     return final_image
 
-def predict(image_path, model, top_k, cat_to_name, is_gpu):   
-    ''' 
+def predict(image_path, model, top_k, cat_to_name, is_gpu):
+    '''
     Predict the class (or classes) of an image using a trained deep learning model.
+    Input image can be from the downloaded dataset 'flowers'
+    or any image that is not part of the dataset, but saved in ideal size and format.
     '''
     #Computation does not require GPU
     #device = "cpu" #for testing purpose
@@ -257,25 +259,31 @@ def predict(image_path, model, top_k, cat_to_name, is_gpu):
     img_torch = img_torch.unsqueeze_(0).float()
     #Make a forward pass with the image and get top-K probabilities and flower labels
     with torch.no_grad():
-        if (device=="cpu"):
-            output = model.forward(img_torch) 
+        if (str(device)=="cpu"):
+            output = model.forward(img_torch)
         elif (is_gpu or str(device)=='cuda'):
             output = model.forward(img_torch.cuda())
     output = torch.exp(output)
     probs, labels = output.topk(top_k)
-    
+
     #Get top flowers from top labels through class_to_idx
-    top_probs = np.array(probs.detach())[0] 
+    top_probs = np.array(probs.detach())[0]
     top_labels = np.array(labels.detach())[0]
     idx_to_class = {value: key for key, value in model.class_to_idx.items()}
     top_labels = [idx_to_class[l] for l in top_labels]
     top_flowers = [cat_to_name[l] for l in top_labels]
-    
-    #Actual name of flower
-    flower_num = image_path.split('/')[2]
-    title = cat_to_name[flower_num]
-    title.replace('_',' ')
-    
+
+    #Only if the sample image is from local flower dataset,
+    #get the actual name of the flower
+    print_verdict = False
+    #It would be ideal to have data_dir from train script here
+    #instead of using the name 'flowers'
+    if(image_path.split('/')[0] == "flowers"):
+        flower_num = image_path.split('/')[2]
+        title = cat_to_name[flower_num]
+        title.replace('_',' ')
+        print_verdict=True
+
     print('\n\n---------------------------------------------')
     print(f'PREDICTION RESULT \nImage : {image_path}')
     print(f'DISPLAY TOP {top_k} MOST PROBABLE FLOWER CATEGORIES\nAND THEIR PROBABILITIES')
@@ -285,10 +293,11 @@ def predict(image_path, model, top_k, cat_to_name, is_gpu):
     for i, j in zip(top_flowers, top_probs):
         print(f'{i:20} :\t {j:.3f}')
     print('---------------------------------------------')
-    print('VERDICT:')
-    if title == top_flowers[0]:
-        print(f'Actual flower name {title.title()} matches the \ntop prediction by our model!')
-    else:
-        print(f'Actual flower name {title.title()} does not match \nthe top prediction by our model!\
-                \nRetrain model for better prediction!')
-    print('---------------------------------------------')
+    if(print_verdict):
+        print('VERDICT:')
+        if title == top_flowers[0]:
+            print(f'Actual flower name {title.title()} matches the \ntop prediction by our model!')
+        else:
+            print(f'Actual flower name {title.title()} does not match \nthe top prediction by our model!\
+                    \nRetrain model for better prediction!')
+        print('---------------------------------------------')
